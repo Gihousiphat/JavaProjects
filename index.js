e0a77f13d0cecc0e1d529c9f19d51ed45c446e4e
Tyler Brauner <tylerbrauner@gmail.com>
2023-11-02 13:19:26 -0500

    Got clear all function to work

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-
+let listCount = 0;
 function createList() {
-    let list = document.createElement('li');
-    list.id = 'list';
-    let close = document.createElement('input');
-    close.id = 'close';
-    close.type = 'button';
-    close.value = 'X';
-    list.textContent = document.getElementById("textbox").value;
-    const startList = document.querySelector('#to-do_list');
-    startList.appendChild(list);
-    startList.appendChild(close);  
+    const orderedList = document.getElementById('to-do_list');
+    const list = document.createElement('li');
+    listCount++;
+    list.id = `list${listCount}`;
+    list.textContent = document.getElementById('textbox').value;
+    const button = document.createElement('input');
+    button.id = `close${listCount}`;
+    button.type = 'button';
+    button.value = 'X';
+    orderedList.appendChild(list);
+    orderedList.appendChild(button);
+    
 }
 
 function clearList() {
+    for (let i = 1; i < listCount + 1; i++) {
     const startList = document.getElementById('to-do_list');
-    const childList = document.getElementById('list');
-    const childButton = document.getElementById('close');
+    const childList = document.getElementById(`list${i}`);
+    const childButton = document.getElementById(`close${i}`);
     startList.removeChild(childButton);
     startList.removeChild(childList);
+    }
+    return listCount = 0;
 }
 
 
