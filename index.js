4a7bf5aae7ede16c690b7ccec23a147e0c765b52
Tyler Brauner <tylerbrauner@gmail.com>
2023-11-02 12:39:00 -0500

    Enabled removing list items

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
 function createList() {
     let list = document.createElement('li');
+    list.id = 'list';
+    let close = document.createElement('input');
+    close.id = 'close';
+    close.type = 'button';
+    close.value = 'X';
     list.textContent = document.getElementById("textbox").value;
     const startList = document.querySelector('#to-do_list');
     startList.appendChild(list);
-    return list;
+    startList.appendChild(close);  
 }
 
-let myValue = '';
-document.getElementById('textbox').value = myValue;
+function clearList() {
+    const startList = document.getElementById('to-do_list');
+    const childList = document.getElementById('list');
+    const childButton = document.getElementById('close');
+    startList.removeChild(childButton);
+    startList.removeChild(childList);
+}
+
+
