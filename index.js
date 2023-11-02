5c21ed9ea95d52fb71d0a735d5bd2360d13fb117
Tyler Brauner <tylerbrauner@gmail.com>
2023-11-02 16:27:28 -0500

    fixed naming system

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-let listCount = 0;
+let listCount = 1;
 function createList() {
     const orderedList = document.getElementById('to-do_list');
     const list = document.createElement('li');
-    listCount++;
-    list.id = `list${listCount}`;
+    const button = document.createElement('input');
+    if (listCount == 1) {
+        list.id = `list${listCount}`;
+        button.id = `list${listCount}`;
+        listCount++;
+    } else {
+        for(let i = 1; i < listCount; i++) {
+            const checkValue = document.getElementById(`list${i}`);
+            if (checkValue == null) {
+                console.log('hello');
+                list.id = `list${i}`;
+                button.id = `close${i}`;
+                break;
+            } else if (i == listCount - 1) {
+                console.log('goodbye');
+                list.id = `list${listCount}`;
+                button.id = `close${listCount}`;
+                listCount++;
+                break; 
+            }
+        }
+    }
+    console.log(listCount);
     list.textContent = document.getElementById('textbox').value;
-    let button = document.createElement('input');
-    button.id = `close${listCount}`;
     button.type = 'button';
     button.value = 'X';
-    button.addEventListener('click', clearListItem);
+    button.addEventListener('click', clearToDo);
     orderedList.appendChild(list);
     orderedList.appendChild(button);
-    
-}
+    return;
+    }
 
 function clearList() {
     for (let i = 1; i < listCount + 1; i++) {
@@ -28,14 +47,16 @@ function clearList() {
 
 
 
-function clearListItem(test) {
+function clearToDo(test) {
     const value = test.target.id;
     const numRegex = /\d+/;
     const num = value.match(numRegex);
     const number = num[0];
+    console.log(number);
     const startList = document.getElementById('to-do_list');
     const childList = document.getElementById(`list${number}`);
     const childButton = document.getElementById(`close${number}`);
     startList.removeChild(childButton);
     startList.removeChild(childList);
+    return;
 }
